87b1c23c773912516c6602ccadc230ce87ec8acb
Smouska <sandragjacinto@gmail.com>
2018-03-29 19:51:47 -0400

    had patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+const _ = require('lodash');
+const express = require ('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-
-var express = require ('express');
-var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -77,6 +77,33 @@ app.delete('/todos/:id', (req, res) => {
             res.status(400).send();
         });
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); //pick the properties that the user will be able to update
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+        .then((todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo});
+        })
+        .catch((error) => {
+            res.status(400).send();
+        })
+})
 
 app.listen(port, () => {
     console.log(`Started at port ${port}`);
