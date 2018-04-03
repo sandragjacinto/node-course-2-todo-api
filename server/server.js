a3434c67d7909d0d92dff52e868c4b5e2c42a9b3
Smouska <sandragjacinto@gmail.com>
2018-04-03 17:22:11 -0400

    add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -131,6 +131,23 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+
+//Post /users/login {email, password}
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password)
+    .then((user) => {
+       return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((error) => {
+        res.status(400).send();
+    })
+    
+});
+
+
 app.listen(port, () => {
     console.log(`Started at port ${port}`);
 });
