c98f25d98826b26257ff89a9181a63a18aa49bd1
Smouska <sandragjacinto@gmail.com>
2018-04-14 17:52:16 -0400

    add DELETET/users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,18 @@ UserSchema.methods.generateAuthToken = function () {
     });
 }; //no arrow function because we need a this keyword for simplicity
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token //could be just token
+            }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
